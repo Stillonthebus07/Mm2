f79099abda6a724ee2f020bc845f5515df9fe47d
Stillonthebus07 <jfloresnflores@gmail.com>
2026-08-05 14:07:02 -0400

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,2 +1,2 @@
-loadstring(game:HttpGet('https://pastebin.com/JpppNkR3'))()
+loadstring(game:HttpGet('https://pastebin.com/raw/JpppNkR3'))()
 loadstring(game:HttpGet('https://cdn.sourceb.in/bins/V1COQYZ5QV/O'))()
