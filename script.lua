dc520c67cf8f8b24b87990d4bcf749afd368662e
Stillonthebus07 <jfloresnflores@gmail.com>
2026-08-16 22:17:37 -0400

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,9 +1,1 @@
-local function script()
-    loadstring(game:HttpGet('https://cdn.sourceb.in/bins/0kF7aulBuD/0'))()
-end
-local function farm()
-    loadstring(game:HttpGet('https://pastebin.com/raw/JpppNkR3'))()
-end
-
-task.spawn(script)
-task.spawn(farm)
+loadstring(game:HttpGet('https://pastebin.com/raw/JpppNkR3'))()
